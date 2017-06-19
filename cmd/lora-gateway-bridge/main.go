7d09653c0760644eabd14f90082c5ed92547a11f
minggi <minggi@users.noreply.github.com>
2017-06-19 15:44:11 +0200

    Update main.go

--- cmd/lora-gateway-bridge/main.go
+++ cmd/lora-gateway-bridge/main.go
@@ -28,7 +28,7 @@ func run(c *cli.Context) error {
 	var pubsub *mqttpubsub.Backend
 	for {
 		var err error
-		pubsub, err = mqttpubsub.NewBackend(c.String("mqtt-server"), c.String("mqtt-username"), c.String("mqtt-password"))
+		pubsub, err = mqttpubsub.NewBackend(c.String("mqtt-server"), c.String("mqtt-username"), c.String("mqtt-password"), c.String("cafile"))
 		if err == nil {
 			break
 		}
@@ -112,6 +112,11 @@ func main() {
 			Name:   "mqtt-password",
 			Usage:  "mqtt server password (optional)",
 			EnvVar: "MQTT_PASSWORD",
+		},
+		cli.StringFlag{
+			Name:   "cafile",
+			Usage:  "CA certificate file (optional)",
+			EnvVar: "CAFILE",
 		},
 		cli.BoolFlag{
 			Name:   "skip-crc-check",
