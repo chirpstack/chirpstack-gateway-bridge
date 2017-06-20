df3a18d17b82ddfc4323fb7f44cf64f1d5269f8a
minggi <minggi@users.noreply.github.com>
2017-06-20 07:15:21 +0200

    Update backend.go

--- backend/mqttpubsub/backend.go
+++ backend/mqttpubsub/backend.go
@@ -22,6 +22,11 @@ type Backend struct {
 
 // NewBackend creates a new Backend.
 func NewBackend(server, username, password string) (*Backend, error) {
+	return NewBackend(server, username, password, "")
+}
+
+// NewBackend creates a new Backend.
+func NewBackend(server, username, password, cafile string) (*Backend, error) {
 	b := Backend{
 		txPacketChan: make(chan gw.TXPacketBytes),
 		gateways:     make(map[lorawan.EUI64]struct{}),
@@ -33,7 +38,14 @@ func NewBackend(server, username, password string) (*Backend, error) {
 	opts.SetPassword(password)
 	opts.SetOnConnectHandler(b.onConnected)
 	opts.SetConnectionLostHandler(b.onConnectionLost)
-
+	
+	if len(cafile) != 0 {
+		tlsconfig := NewTLSConfig(cafile)
+		if(tlsconfig != nil) {
+			opts.SetClientID("ssl-client").SetTLSConfig(tlsconfig)
+		}
+	}
+	
 	log.WithField("server", server).Info("backend: connecting to mqtt broker")
 	b.conn = mqtt.NewClient(opts)
 	if token := b.conn.Connect(); token.Wait() && token.Error() != nil {
@@ -41,6 +53,25 @@ func NewBackend(server, username, password string) (*Backend, error) {
 	}
 
 	return &b, nil
+}
+
+func NewTLSConfig(cafile string) (*tls.Config, error) {
+	// Import trusted certificates from CAfile.pem.
+	
+	cert, err := ioutil.ReadFile(cafile)
+	if err != nil {
+		log.Errorf("backend: couldn't load cafile", err)
+		return nil, err
+	}
+	
+	certpool := x509.NewCertPool()
+	certpool.AppendCertsFromPEM(cert)
+	
+	// Create tls.Config with desired tls properties
+	return &tls.Config{
+		// RootCAs = certs used to verify server cert.
+		RootCAs: certpool,
+	}, nil
 }
 
 // Close closes the backend.
