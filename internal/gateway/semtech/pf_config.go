3465385c5ea3602364e1f63ebe56776e75e168aa
gq-tang <tgq2004@163.com>
2018-08-28 17:43:26 +0800

    Fix MultiSFChannels index out of range

--- internal/gateway/semtech/pf_config.go
+++ internal/gateway/semtech/pf_config.go
@@ -244,7 +244,7 @@ func getGatewayConfig(conf gw.GatewayConfiguration) (gatewayConfiguration, error
 				}
 			} else {
 				// LoRa multi-SF channels
-				if multiSFCounter > channelCount {
+				if multiSFCounter >= channelCount {
 					return gc, errors.New("gateway: exceeded maximum number of multi-sf channels")
 				}
 
