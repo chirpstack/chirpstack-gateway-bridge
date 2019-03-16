1fb73db2569e4885887edac075132abf5459ab38
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2019-03-16 22:38:19 +0000

    Improves struct layout.

--- internal/gateway/semtech/packets/pull_resp.go
+++ internal/gateway/semtech/packets/pull_resp.go
@@ -63,22 +63,22 @@ type PullRespPayload struct {
 // TXPK contains a RF packet to be emitted and associated metadata.
 type TXPK struct {
 	Imme bool    `json:"imme"`           // Send packet immediately (will ignore tmst & time)
+	RFCh uint8   `json:"rfch"`           // Concentrator "RF chain" used for TX (unsigned integer)
+	Powe uint8   `json:"powe"`           // TX output power in dBm (unsigned integer, dBm precision)
+	Ant  uint8   `json:"ant"`            // Antenna number on which signal has been received
+	Brd  uint32  `json:"brd"`            // Concentrator board used for RX (unsigned integer)
 	Tmst *uint32 `json:"tmst,omitempty"` // Send packet on a certain timestamp value (will ignore time)
 	Tmms *int64  `json:"tmms,omitempty"` // Send packet at a certain GPS time (GPS synchronization required)
 	Freq float64 `json:"freq"`           // TX central frequency in MHz (unsigned float, Hz precision)
-	RFCh uint8   `json:"rfch"`           // Concentrator "RF chain" used for TX (unsigned integer)
-	Powe uint8   `json:"powe"`           // TX output power in dBm (unsigned integer, dBm precision)
 	Modu string  `json:"modu"`           // Modulation identifier "LORA" or "FSK"
 	DatR DatR    `json:"datr"`           // LoRa datarate identifier (eg. SF12BW500) || FSK datarate (unsigned, in bits per second)
 	CodR string  `json:"codr,omitempty"` // LoRa ECC coding rate identifier
 	FDev uint16  `json:"fdev,omitempty"` // FSK frequency deviation (unsigned integer, in Hz)
+	NCRC bool    `json:"ncrc,omitempty"` // If true, disable the CRC of the physical layer (optional)
 	IPol bool    `json:"ipol"`           // Lora modulation polarization inversion
 	Prea uint16  `json:"prea,omitempty"` // RF preamble size (unsigned integer)
 	Size uint16  `json:"size"`           // RF packet payload size in bytes (unsigned integer)
-	NCRC bool    `json:"ncrc,omitempty"` // If true, disable the CRC of the physical layer (optional)
 	Data []byte  `json:"data"`           // Base64 encoded RF packet payload, padding optional
-	Brd  uint32  `json:"brd"`            // Concentrator board used for RX (unsigned integer)
-	Ant  uint8   `json:"ant"`            // Antenna number on which signal has been received
 }
 
 // GetPullRespPacket returns a PullRespPacket for the given gw.DownlinkFrame.
